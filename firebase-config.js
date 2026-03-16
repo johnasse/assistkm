bcfd24a52d3a69ba9d6b327b274f6c993b0c2ed3
johnasse <john.rombert.76@gmail.com>
2026-03-16 21:06:24 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,20 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
 
-// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCc9uGltdHFmKmVOcqIYAY7nD6qHnykeo",
+  apiKey: "AIzaSyCc9uGltdHfmKmnVOcqIYAY7nD6qHnykeo",
   authDomain: "assistkm-24d0a.firebaseapp.com",
+  databaseURL: "https://assistkm-24d0a-default-rtdb.firebaseio.com",
   projectId: "assistkm-24d0a",
-  storageBucket: "assistkm-24d0a.appspot.com",
+  storageBucket: "assistkm-24d0a.firebasestorage.app",
   messagingSenderId: "172856206943",
-  appId: "1:172856206943:web:ebdaa4a9a97130b7721dea"
+  appId: "1:172856206943:web:ebdaa4a9a97130b7721dea",
+  measurementId: "G-PJ582TYYFG"
 };
 
-// Initialisation Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+
+const analytics = getAnalytics(app);
