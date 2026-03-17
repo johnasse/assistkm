825ab9558886bf13a6dc484a88bb4d2bea79cbb6
johnasse <john.rombert.76@gmail.com>
2026-03-17 23:20:09 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+console.log("FIREBASE VERSION 999");
 import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
