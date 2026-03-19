bd2612affb417a62af35fb74b33ce0e7a8e0b6c3
johnasse <john.rombert.76@gmail.com>
2026-03-19 21:00:53 +0100

    fix firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-console.log("FIREBASE VERSION 999");
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
-import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCc9uGltdHfmKmnVOcqIYAY7nD6qHnykeo",
   authDomain: "assistkm-24d0a.firebaseapp.com",
-  databaseURL: "https://assistkm-24d0a-default-rtdb.firebaseio.com",
   projectId: "assistkm-24d0a",
   storageBucket: "assistkm-24d0a.firebasestorage.app",
   messagingSenderId: "172856206943",
@@ -15,16 +14,8 @@ const firebaseConfig = {
   measurementId: "G-PJ582TYYFG"
 };
 
-export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// 🔥 IMPORTANT
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-try {
-  isSupported().then((ok) => {
-    if (ok) {
-      getAnalytics(app);
-    }
-  });
-} catch (error) {
-  console.warn("Analytics non initialisé :", error);
-}
